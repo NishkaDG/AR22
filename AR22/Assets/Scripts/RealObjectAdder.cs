18464a4ac7f273091cd4439b33b637fa2786dfda
Nishka <nishka@post.au.dk>
2022-02-09 18:34:56 +0100

    2.1 first attempt
    Signed-off-by: Nishka <nishka@post.au.dk>

--- AR22/Assets/Scripts/RealObjectAdder.cs
+++ AR22/Assets/Scripts/RealObjectAdder.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+	[SerializeField]
+	public GameObject cubeToDrop;
+	// Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +15,20 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		Ray myRay;
+		RaycastHit hit;
+		Touch touch;
+		for(int i = 0; i < Input.touchCount; i++) {
+			touch = Input.GetTouch(i);
+			if (touch.phase.Equals(TouchPhase.Began)) {
+				myRay = Camera.main.ScreenPointToRay(touch.position);
+				if(Physics.Raycast(myRay, out hit)) {
+					//Vector3 dropPoint = hit.point + new Vector3(0, 0.1f, 0);
+					Vector3 dropPoint = myRay.GetPoint(hit.distance);
+					GameObject newCube = Instantiate(cubeToDrop, dropPoint, Quaternion.identity);
+				}
+			}
+		}
         
     }
 }
