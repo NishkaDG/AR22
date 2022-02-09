4463f3ec62077b6311ce9bcf1d03b9ddebeaa375
Rasmus Thorsøe <rt071195@gmail.com>
2022-02-09 16:59:17 +0100

    Assignment 2: Exercise 2.1

--- AR22/Assets/Scripts/RealObjectAdder.cs
+++ AR22/Assets/Scripts/RealObjectAdder.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RealObjectAdder : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] 
+    private GameObject cubePrefab;
+    
+    [SerializeField] 
+    private Transform parent;
+    
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase != TouchPhase.Began)
+            {
+                return;
+            }
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            if (Physics.Raycast(ray, out hit)) {
+                Vector3 point = hit.point;
+                var thing = Instantiate(cubePrefab, point, new Quaternion());
+                // Do something with the object that was hit by the raycast.
+            }
+        }
     }
 }
