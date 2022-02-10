6968836b05278e69f694ce6f4cbe45303d5fea79
Sam Martin Vargas Giagnocavo <samfefuty@hotmail.com>
2022-02-10 21:37:30 +0100

    Replace cube with new asset

--- AR22/Assets/Scripts/RealObjectAdder.cs
+++ AR22/Assets/Scripts/RealObjectAdder.cs
@@ -23,18 +23,25 @@ public class RealObjectAdder : MonoBehaviour
     GameObject cubeObj;
     Rigidbody rigidBody;
     
+    Vector3 raycastPos, placePos;
+    
     // Start is called before the first frame update
     void Start() {
         camera = Camera.main;
         activePlaceholder = null;     
     }
+    
+    public void AddCube() {
+        if (activePlaceholder) {
+            Instantiate(realCube, raycastPos, Quaternion.identity);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         
         Ray ray;
-        Touch touch;
-        Vector3 raycastPos, placePos;
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         
         // SHOULD WE POSITION THE PLACEHOLDER ?
@@ -45,33 +52,9 @@ public class RealObjectAdder : MonoBehaviour
             raycastPos = ray.GetPoint(hits[0].distance);
              // We want to position it underneath the plane
             if (activePlaceholder) {
-                Debug.Log("WE SHOULD MOVE THE PLACEHOLDER");
                 activePlaceholder.transform.position = raycastPos;
             } else {
-                Debug.Log("WE SHOULD CREATE THE PLACEHOLDER");
                 activePlaceholder = Instantiate(placeholder, raycastPos, Quaternion.identity);
-            }
-
-            // PLACING CUBES
-            if (Input.touchCount > 0) {
-
-                touch = Input.GetTouch(0);
-                
-                if (touch.phase == TouchPhase.Began) {
-                    
-                    Instantiate(realCube, raycastPos, Quaternion.identity);
-                    
-                    /* ray = camera.ScreenPointToRay(touch.position); */
-                    
-                    /* if (raycastManager.Raycast(touch.position, hits)) { */
-                    /*     raycastPos = ray.GetPoint(hits[0].distance); */
-                        
-                    /*     // We have to add an offset */
-                    /*     /1* raycastPos.y += 2; *1/ */
-
-                    /*     Instantiate(realCube, raycastPos, Quaternion.identity); */
-                    /* } */
-                }
             }
         } else if (activePlaceholder) {
             Destroy(activePlaceholder);
