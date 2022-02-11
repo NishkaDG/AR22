ee31cb205500734d025516b163aea912ae093e82
Sam Martin Vargas Giagnocavo <samfefuty@hotmail.com>
2022-02-11 18:25:32 +0100

    Fix exercise 3.2

--- AR22/Assets/Scripts/TrackedObject.cs
+++ AR22/Assets/Scripts/TrackedObject.cs
@@ -12,9 +12,6 @@ public class TrackedObject : MonoBehaviour
     [SerializeField]
     GameObject prefab;
 
-    [SerializeField]
-    GameObject prefabXL;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +22,14 @@ public class TrackedObject : MonoBehaviour
     {
         foreach (var newImage in eventArgs.added)
         {
-            Debug.Log("NEW IMAGE :D");
-            Debug.Log(newImage.referenceImage.name);
-            if (newImage.referenceImage.name == "refimage-1") {
-                Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity, newImage.transform);
-            } else if (newImage.referenceImage.name == "refimage-2") {
-                Instantiate(prefabXL, new Vector3(0, 0, 0), Quaternion.identity, newImage.transform);
-            }
+            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity, newImage.transform);
+            
+            // THIS CONDITIONAL SHOULD BE USED IN CASE OF INSERTING DIFFERENT PREFABS  
+            /* if (newImage.referenceImage.name == "refimage-1") { */
+            /*     Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity, newImage.transform); */
+            /* } else if (newImage.referenceImage.name == "refimage-2") { */
+            /*     Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity, newImage.transform); */
+            /* } */
         }
 
         foreach (var updatedImage in eventArgs.updated)
