f8ead329ba7ab6d75e6f1bc8cc139cc28157b0da
Rasmus Thorsøe <rt071195@gmail.com>
2022-02-13 18:08:56 +0100

    Assignment 2: Exercise 2.4

--- AR22/Assets/Scripts/RealObjectAdder.cs
+++ AR22/Assets/Scripts/RealObjectAdder.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.XR.ARFoundation;
 
 public class RealObjectAdder : MonoBehaviour
@@ -13,52 +14,66 @@ public class RealObjectAdder : MonoBehaviour
     
     [SerializeField]
     GameObject placeholder;
-    GameObject activePlaceholder;
+    
+    [SerializeField]
+    Renderer placeholderRenderer;
+    private bool showingPlaceholder = false;
 
     [SerializeField]
     ARRaycastManager raycastManager;
     
-    // GameObject we are going to create 
-    // and its Rigidbody
-    GameObject cubeObj;
-    Rigidbody rigidBody;
-    
-    Vector3 raycastPos, placePos;
     
     // Start is called before the first frame update
     void Start() {
         camera = Camera.main;
-        activePlaceholder = null;     
+        Debug.Log("test");
     }
     
-    public void AddCube() {
-        if (activePlaceholder) {
-            Instantiate(prefab, raycastPos, Quaternion.identity);
+    public void AddObject(Vector3 position, Quaternion rotation) {
+        if (showingPlaceholder) {
+            Instantiate(prefab, position, rotation);
         }
+    }
+
+    public void ShowPlaceholder()
+    {
+        showingPlaceholder = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Ray ray;
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        
-        // SHOULD WE POSITION THE PLACEHOLDER ?
-        placePos = camera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
-        ray = camera.ScreenPointToRay(placePos);
-
-        if (raycastManager.Raycast(placePos, hits)) {
-            raycastPos = ray.GetPoint(hits[0].distance);
-             // We want to position it underneath the plane
-            if (activePlaceholder) {
-                activePlaceholder.transform.position = raycastPos;
-            } else {
-                activePlaceholder = Instantiate(placeholder, raycastPos, Quaternion.identity);
-            }
-        } else if (activePlaceholder) {
-            Destroy(activePlaceholder);
+        if (!showingPlaceholder)
+        {
+            placeholderRenderer.enabled = false;
+            return;
         }
+        
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        // Move the placeholder
+        var placeHolderPosition = camera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
 
+        if (raycastManager.Raycast(placeHolderPosition, hits)) {
+            var hit = hits[0];
+            if (hit.trackable is ARPlane plane)
+            {
+                placeholderRenderer.enabled = true;
+                placeholder.transform.position = hit.pose.position;
+                placeholder.transform.rotation = hit.pose.rotation;
+            }
+            
+            // check for input to place object
+            if (Input.touchCount > 0) {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase != TouchPhase.Began)
+                {
+                    return;
+                }
+                AddObject(placeholder.transform.position, placeholder.transform.rotation);
+                showingPlaceholder = false;
+            }
+        } else {
+            placeholderRenderer.enabled = false;
+        }
     }
 }
