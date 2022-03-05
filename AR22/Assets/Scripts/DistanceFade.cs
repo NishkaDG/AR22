f9e6ad8f09276d356b3574ac50486cbe73f845d7
Rasmus Thorsøe <rt071195@gmail.com>
2022-03-05 11:05:00 +0100

    assignment 5: change tag and dist, add script to object

--- AR22/Assets/Scripts/DistanceFade.cs
+++ AR22/Assets/Scripts/DistanceFade.cs
@@ -10,19 +10,19 @@ public class DistanceFade : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+	    
     }
 
     // Update is called once per frame
     void Update()
     {
-		GameObject[] go = GameObject.FindGameObjectsWithTag("art");
+		GameObject[] go = GameObject.FindGameObjectsWithTag("poster");
 		Vector3 campos = Camera.main.transform.position;
 		for (int i = 0; i < go.Length; i++) {
 			GameObject obj = go[i];
 			var dist = Vector3.Distance(obj.transform.position, campos);
-			if(dist < 0.1) {
-				transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
+			if(dist < 0.7) {
+				obj.transform.RotateAround(obj.transform.position, obj.transform.up, Time.deltaTime * 90f);
 			}
 		}
     }
